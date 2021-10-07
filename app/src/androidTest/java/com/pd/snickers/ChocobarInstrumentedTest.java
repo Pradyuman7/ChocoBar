dcb66ab8818b429e6849f40b0cee2dd87568d1f2
James Ellerbee <jamesellerbee1@gmail.com>
2021-10-06 22:17:35 -0500

    chocobar instrumented tests, modified gradle properties.

---
+++ app/src/androidTest/java/com/pd/snickers/ChocobarInstrumentedTest.java
@@ -0,0 +1,320 @@
+package com.pd.snickers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
+import android.support.design.widget.Snackbar;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
+
+import com.pd.chocobar.ChocoBar;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests {@link ChocoBar}.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ChocobarInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    /**
+     * Verifies the default Chocobar type.
+     */
+    @Test
+    public void testDefaultChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a default Snackbar
+        Snackbar chocoBar = chocoBarBuilder.build();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals("Snackbar text did not match the expected.", "", snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.", Snackbar.LENGTH_SHORT, chocoBar.getDuration());
+
+        assertTrue(chocolateLayout.getBackground() instanceof GradientDrawable);
+    }
+
+    /**
+     * Verifies the green Chocobar type.
+     */
+    @Test
+    public void testGreenChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a green Snackbar
+        Snackbar chocoBar = chocoBarBuilder.green();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "SUCCESS !",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #388E3C.",
+                Color.parseColor("#388E3C"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the red Chocobar type.
+     */
+    @Test
+    public void testRedChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a red Snackbar
+        Snackbar chocoBar = chocoBarBuilder.red();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "ERROR !",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #D50000.",
+                Color.parseColor("#D50000"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the cyan Chocobar type.
+     */
+    @Test
+    public void testCyanChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a cyan Snackbar
+        Snackbar chocoBar = chocoBarBuilder.cyan();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "CYAN",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #e0ffff.",
+                Color.parseColor("#e0ffff"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the orange Chocobar type.
+     */
+    @Test
+    public void testOrangeChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a orange Snackbar
+        Snackbar chocoBar = chocoBarBuilder.orange();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "WARNING !",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #ffa500.",
+                Color.parseColor("#ffa500"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the good Chocobar type.
+     */
+    @Test
+    public void testGoodChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a good Snackbar
+        Snackbar chocoBar = chocoBarBuilder.good();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "GRAY_GOOD",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #C5BEBE.",
+                Color.parseColor("#C5BEBE"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the bad Chocobar type.
+     */
+    @Test
+    public void testBadChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a bad Snackbar
+        Snackbar chocoBar = chocoBarBuilder.bad();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "GRAY_BAD",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #C5BEBE.",
+                Color.parseColor("#C5BEBE"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the bad Chocobar type.
+     */
+    @Test
+    public void testBlackChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a black Snackbar
+        Snackbar chocoBar = chocoBarBuilder.black();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "Black",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #000000.",
+                Color.parseColor("#000000"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the love Chocobar type.
+     */
+    @Test
+    public void testLoveChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a black Snackbar
+        Snackbar chocoBar = chocoBarBuilder.love();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "LOVE",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #E8290B.",
+                Color.parseColor("#E8290B"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+
+    /**
+     * Verifies the love Chocobar type.
+     */
+    @Test
+    public void testNotificationOnChocobar() {
+        // Given chocobar builder
+        ChocoBar.Builder chocoBarBuilder = ChocoBar.builder()
+                .setView(activityTestRule.getActivity().getCurrentFocus());
+
+        // When building a black Snackbar
+        Snackbar chocoBar = chocoBarBuilder.notificationsOn();
+
+        // Then it matches the expected configuration
+        Snackbar.SnackbarLayout chocolateLayout = (Snackbar.SnackbarLayout) chocoBar.getView();
+
+        TextView snackbarText = (TextView) chocolateLayout.findViewById(R.id.snackbar_text);
+        assertEquals(
+                "Snackbar text did not match the expected.",
+                "NOTIFICATIONS ON",
+                snackbarText.getText());
+
+        assertEquals("Snackbar duration did not match the expected.",
+                Snackbar.LENGTH_SHORT,
+                chocoBar.getDuration());
+
+        assertEquals("Expected background color for snackbar layout to be #000000.",
+                Color.parseColor("#000000"),
+                ((ColorDrawable) chocolateLayout.getBackground()).getColor());
+    }
+}
--- app/src/main/java/com/pd/snickers/MainActivity.java
+++ app/src/main/java/com/pd/snickers/MainActivity.java
@@ -19,91 +19,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button_default).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setActivity(MainActivity.this).setActionText("ACTION").setActionClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(MainActivity.this, "You clicked", Toast.LENGTH_LONG).show();
-                    }
-                }).setText("This is a normal ChocoBar").setDuration(ChocoBar.LENGTH_INDEFINITE).build().show();
-            }
-        });
+        findViewById(R.id.button_default).setOnClickListener(v -> ChocoBar.builder()
+                .setActivity(MainActivity.this).setActionText("ACTION")
+                .setActionClickListener(v1 -> Toast.makeText(MainActivity.this, "You clicked", Toast.LENGTH_LONG)
+                        .show())
+                .setText("This is a normal ChocoBar")
+                .setDuration(ChocoBar.LENGTH_INDEFINITE).build()
+                .show());
 
-        findViewById(R.id.button_success).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setActivity(MainActivity.this).setDuration(ChocoBar.LENGTH_SHORT).green().show();
-            }
-        });
+        findViewById(R.id.button_success).setOnClickListener(v -> ChocoBar
+                .builder()
+                .setActivity(MainActivity.this)
+                .setDuration(ChocoBar.LENGTH_SHORT)
+                .green()
+                .show());
 
-        findViewById(R.id.button_info).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar warningSnackBar = ChocoBar.builder().setActivity(MainActivity.this).setDuration(ChocoBar.LENGTH_LONG).orange();
-                warningSnackBar.show();
-            }
-        });
+        findViewById(R.id.button_info).setOnClickListener(v -> ChocoBar.builder()
+                .setActivity(MainActivity.this)
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .orange()
+                .show());
 
-        findViewById(R.id.button_warning).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).centerText().setDuration(ChocoBar.LENGTH_LONG).cyan().show();
-            }
-        });
+        findViewById(R.id.button_warning).setOnClickListener(v -> ChocoBar
+                .builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .cyan()
+                .show());
 
-        findViewById(R.id.button_error).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).setDuration(ChocoBar.LENGTH_INDEFINITE).setActionText(android.R.string.ok).red().show();
-            }
-        });
+        findViewById(R.id.button_error).setOnClickListener(v -> ChocoBar
+                .builder()
+                .setView(v)
+                .setDuration(ChocoBar.LENGTH_INDEFINITE)
+                .setActionText(android.R.string.ok)
+                .red()
+                .show());
 
-        findViewById(R.id.button_good).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).centerText().setDuration(ChocoBar.LENGTH_LONG).good().show();
-            }
-        });
+        findViewById(R.id.button_good).setOnClickListener(v -> ChocoBar.builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .good()
+                .show());
 
-        findViewById(R.id.button_bad).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).centerText().setDuration(ChocoBar.LENGTH_LONG).bad().show();
-            }
-        });
+        findViewById(R.id.button_bad).setOnClickListener(v -> ChocoBar.builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .bad()
+                .show());
 
-        findViewById(R.id.button_love).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).centerText().setDuration(ChocoBar.LENGTH_LONG).love().show();
-            }
-        });
+        findViewById(R.id.button_love).setOnClickListener(v -> ChocoBar.builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .love()
+                .show());
 
-        findViewById(R.id.button_custom).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setBackgroundColor(Color.parseColor("#00bfff")).setTextSize(18).setTextColor(Color.parseColor("#FFFFFF")).setTextTypefaceStyle(Typeface.ITALIC).setText("This is a custom Chocobar").setMaxLines(4).centerText().setActionText("ChocoBar").setActionTextColor(Color.parseColor("#66FFFFFF")).setActionTextSize(20).setActionTextTypefaceStyle(Typeface.BOLD).setIcon(R.mipmap.ic_launcher).setActivity(MainActivity.this).setDuration(ChocoBar.LENGTH_INDEFINITE).build().show();
-            }
-        });
+        findViewById(R.id.button_custom).setOnClickListener(v -> ChocoBar.builder()
+                .setBackgroundColor(Color.parseColor("#00bfff"))
+                .setTextSize(18)
+                .setTextColor(Color.parseColor("#FFFFFF"))
+                .setTextTypefaceStyle(Typeface.ITALIC)
+                .setText("This is a custom Chocobar")
+                .setMaxLines(4)
+                .centerText()
+                .setActionText("ChocoBar")
+                .setActionTextColor(Color.parseColor("#66FFFFFF"))
+                .setActionTextSize(20)
+                .setActionTextTypefaceStyle(Typeface.BOLD)
+                .setIcon(R.mipmap.ic_launcher)
+                .setActivity(MainActivity.this)
+                .setDuration(ChocoBar.LENGTH_INDEFINITE)
+                .build()
+                .show());
 
-        findViewById(R.id.button_off_notification).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder().setView(v).centerText().setDuration(ChocoBar.LENGTH_LONG).black().show();
-            }
-        });
+        findViewById(R.id.button_off_notification).setOnClickListener(v -> ChocoBar.builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .black()
+                .show());
 
-        findViewById(R.id.button_on_notification).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ChocoBar.builder()
-                        .setView(v)
-                        .centerText()
-                        .setDuration(ChocoBar.LENGTH_LONG)
-                        .notificationsOn().show();
-            }
-        });
+        findViewById(R.id.button_on_notification).setOnClickListener(v -> ChocoBar.builder()
+                .setView(v)
+                .centerText()
+                .setDuration(ChocoBar.LENGTH_LONG)
+                .notificationsOn().show());
     }
 }
