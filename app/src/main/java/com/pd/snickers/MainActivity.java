5b465c4d43da453d8dcfba290c1dfbbc7435b18e
firecatmusic <nodelay200@gmail.com>
2023-12-11 13:43:49 +0700

    - Migrating to AndroidX, - Fix build.gradle level project

--- app/src/main/java/com/pd/snickers/MainActivity.java
+++ app/src/main/java/com/pd/snickers/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.Toast;
 
 
--- chocobar/src/main/java/com/pd/chocobar/ChocoBarUtils.java
+++ chocobar/src/main/java/com/pd/chocobar/ChocoBarUtils.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorInt;
-import android.support.annotation.NonNull;
-import android.support.v4.graphics.drawable.DrawableCompat;
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 
 public class ChocoBarUtils {
